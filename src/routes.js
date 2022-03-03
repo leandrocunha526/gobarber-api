6b27abd2c30669da6f0f248abf85fab3b9ceee46
leandrocunha526 <leandrocunha016@gmail.com>
2022-03-03 16:57:07 -0300

    Add routes.js

---
+++ src/routes.js
@@ -0,0 +1,53 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import authMiddleware from './app/middlewares/auth';
+
+// Users
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+// Providers
+import ProviderController from './app/controllers/ProviderController';
+import AvailableController from './app/controllers/AvailableController';
+
+// Appointment;
+import AppointmentController from './app/controllers/AppointmentController';
+
+// Schedule
+import ScheduleController from './app/controllers/ScheduleController';
+
+// Notifications
+import NotificationController from './app/controllers/NotificationController';
+
+// Files
+import FileController from './app/controllers/FileController';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+// Users
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.use(authMiddleware);
+Router.put('/users', UserController.update);
+
+// Providers
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+// Appointment
+routes.post('/providers', AppointmentController.store);
+routes.get('/providers', AppointmentController.index);
+routes.delete('/providers', AppointmentController.delete);
+
+// Schedule
+routes.post('/schedule', ScheduleController.store);
+
+// Notification
+routes.get('/notification', NotificationController.index);
+routes.put('/notification/:id', NotificationController.update);
+
+// Files
+routes.post('/files', upload.single('file'), FileController.store);
